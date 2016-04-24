3afac0623da2c51a07eb89b5809fa52312cf8638
BellaT <bella_taylor@hotmail.co.uk>
2016-04-24 15:24:07 +0100

    added the favourites model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'favourites/index'
+
+  get 'favourites/new'
+
+  get 'favourites/show'
+
+  get 'favourites/create'
+
+  get 'favourites/edit'
+
+  get 'favourites/update'
+
+  get 'favourites/destroy'
+
   root 'statics#home'
   devise_for :users
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423200451) do
+ActiveRecord::Schema.define(version: 20160424142208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "festival_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "favourites", ["festival_id"], name: "index_favourites_on_festival_id", using: :btree
+  add_index "favourites", ["user_id"], name: "index_favourites_on_user_id", using: :btree
 
   create_table "festivals", force: :cascade do |t|
     t.string   "title"
@@ -53,4 +63,6 @@ ActiveRecord::Schema.define(version: 20160423200451) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "favourites", "festivals"
+  add_foreign_key "favourites", "users"
 end
