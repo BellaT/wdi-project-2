a75e2d8e2ef0680392d9d24b3f5bedda82ffb623
BellaT <bella_taylor@hotmail.co.uk>
2016-04-26 15:52:41 +0100

    got parallax and image uploader all working on the festival show page.

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -2,5 +2,7 @@ class Festival < ActiveRecord::Base
   has_many :favourites
 
   scope :search_by_month, ->(month) { where('extract(month from festival_date) = ?', Date::MONTHNAMES.index(month))}
-  mount_uploader :image, GraffitiImageUploader
+  mount_uploader :main_image, FestivalImageUploader
+  mount_uploader :second_image, FestivalImageUploader
+  mount_uploader :third_image, FestivalImageUploader
 end
